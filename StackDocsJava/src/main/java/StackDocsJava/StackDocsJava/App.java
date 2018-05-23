d9f3d0f494f03e68e01477594364fb798474ccfc
Neimantas <p.neimantas@gmail.com>
2018-05-23 13:50:10 +0300

    StackDocsJava main files.

---
+++ StackDocsJava/src/main/java/StackDocsJava/StackDocsJava/App.java
@@ -0,0 +1,13 @@
+package StackDocsJava.StackDocsJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
