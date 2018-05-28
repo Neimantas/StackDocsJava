3e4f2fd4b8e85ad6b16dc0f62e1c0dfc0e2cf51c
CrossPC\Karolis <karolis.vas@gmail.com>
2018-05-28 14:16:20 +0300

    CRUD updated create update and delete methods.

--- StackDocsJava/src/main/Services/ICrud.java
+++ StackDocsJava/src/main/Services/ICrud.java
@@ -1,13 +1,16 @@
 package Services;
 
 import java.sql.ResultSet;
-import java.util.List;
 
 public interface ICrud {
-	
-	void create(String tableName);
+
+	void create(String tableName, String[] values);
+
 	ResultSet read(String tableName);
-	void update(String tableName, String renamedTableName);
-	void delete(String tableName);
+
+	void update(String tableName, String changeValueOfColum, String changeValueTO, String conditionColumName,
+			String conditionChangeWhereValueIsEqual);
+
+	void delete(String tableName, String conditionColum, String conditionValue);
 
 }
--- StackDocsJava/src/main/Services/Impl/CRUD.java
+++ StackDocsJava/src/main/Services/Impl/CRUD.java
@@ -1,14 +1,9 @@
 package Services.Impl;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-
-import Models.DAL.LanguageTagDAL;
 import Services.ICrud;
 import Services.IDataBase;
 
@@ -29,13 +24,22 @@ public class CRUD implements ICrud {
 		}
 	}
 
-	public void create(String tableName) {
-		String readQuerry = "CREATE TABLE " + tableName + ";";
+	public void create(String tableName, String[] values) {
+
+		String fullValuesString = "";
+		for (int i = 0; i < values.length; i++) {
+			fullValuesString.concat(values[i]);
+			fullValuesString.concat(",");
+		}
+
+		fullValuesString.equals(fullValuesString.substring(0, fullValuesString.length() - 1));
+
+		String readQuerry = "INSERT INTO " + tableName + "(" + fullValuesString + ");";
 
 		try {
 			readResultSet = statements.executeQuery(readQuerry);
 		} catch (SQLException e) {
-			
+
 			e.printStackTrace();
 		}
 
@@ -48,33 +52,36 @@ public class CRUD implements ICrud {
 		try {
 			readResultSet = statements.executeQuery(readQuerry);
 		} catch (SQLException e) {
-			
+
 			e.printStackTrace();
 		}
 
 		return readResultSet;
 	}
 
-	public void update(String originalTableName, String renamedTableName) {
-//		String readQuerry = "ALTER TABLE " + originalTableName + " RENAME " + renamedTableName + ";";
-//
-//		try {
-//			readResultSet = statements.executeQuery(readQuerry);
-//		} catch (SQLException e) {
-//
-//			e.printStackTrace();
-//		}
+	public void update(String tableName, String changeValueOfColum, String changeValueTO, String conditionColumName,
+			String conditionChangeWhereValueIsEqual) {
+
+		String readQuerry = "UPDATE " + tableName + " SET " + changeValueOfColum + "='" + changeValueTO + "' WHERE"
+				+ conditionColumName + "='" + conditionChangeWhereValueIsEqual + "';";
+
+		try {
+			readResultSet = statements.executeQuery(readQuerry);
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
 	}
 
-	public void delete(String tableName) {
-//		String readQuerry = "DROP TABLE " + tableName + ";";
-//
-//		try {
-//			statements.executeQuery(readQuerry);
-//		} catch (SQLException e) {
-//			
-//			e.printStackTrace();
-//		}
+	public void delete(String tableName, String conditionColum, String conditionValue) {
+		String readQuerry = "DELETE FROM " + tableName + " WHERE " + conditionColum + "='" + conditionValue + "';";
+
+		try {
+			statements.executeQuery(readQuerry);
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
 
 	}
 
