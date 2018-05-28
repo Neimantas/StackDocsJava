dbf083774ae3d98e59ea601f5ea249ad9499f9f7
mmd391 <mmd391@gmail.com>
2018-05-28 13:59:16 +0300

    implemented methods getTopicByLanguageId and getExamplesByTopicId

--- StackDocsJava/src/test/CRUD_Main.java
+++ StackDocsJava/src/test/CRUD_Main.java
@@ -3,6 +3,8 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import Models.DAL.ExamplesDAL;
+import Models.DAL.TopicsDAL;
 import Services.IHigherService;
 import Services.Impl.CRUD;
 import Services.Impl.HigherServiceImpl;
@@ -16,9 +18,17 @@ public class CRUD_Main {
 
 		IHigherService serv = new HigherServiceImpl();
 		
-		System.out.println(serv.readLanguageTag("Bla").size());
-		System.out.println(serv.readTopics("").size());
-		System.out.println(serv.readExamples("").size());
+//		System.out.println(serv.readLanguageTag("Bla").size());
+//		System.out.println(serv.readTopics("").size());
+//		System.out.println(serv.readExamples("").size());
+//		for(int i = 0; i<10; i++) {
+//			TopicsDAL t = (TopicsDAL) serv.getTopicsByLanguageId(5).get(i);
+//			System.out.println(t.title);
+//		}
+		for(int i = 0; i<10; i++) {
+			ExamplesDAL t = (ExamplesDAL) serv.getExamplesByTopicId(1).get(i);
+			System.out.println(t.title);
+		}
 		
 	}
 
