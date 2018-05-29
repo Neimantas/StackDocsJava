7a46c572e23065ecfa1d6b94c32a919e75a2cbb5
CrossPC\Karolis <karolis.vas@gmail.com>
2018-05-29 12:32:30 +0300

    CRUD read error message return added

--- StackDocsJava/src/main/Services/ICrud.java
+++ StackDocsJava/src/main/Services/ICrud.java
@@ -2,11 +2,13 @@ package Services;
 
 import java.sql.ResultSet;
 
+import Models.DTO.ReadTableDTO;
+
 public interface ICrud {
 
 	void create(String tableName, String[] values);
 
-	ResultSet read(String tableName);
+	ReadTableDTO read(String tableName);
 
 	void update(String tableName, String changeValueOfColum, String changeValueTO, String conditionColumName,
 			String conditionChangeWhereValueIsEqual);
