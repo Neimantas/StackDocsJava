6fda4f29f70885733b71d3d5ffc51c47c0cd5b34
CrossPC\Karolis <karolis.vas@gmail.com>
2018-05-31 12:13:46 +0300

    Crud main repair :)

--- StackDocsJava/src/test/CRUD_Main.java
+++ StackDocsJava/src/test/CRUD_Main.java
@@ -3,15 +3,9 @@
 import java.util.ArrayList;
 import java.util.List;
 
-<<<<<<< HEAD
-import Models.DAL.ExamplesDAL;
-import Models.DAL.TopicsDAL;
-import Models.DTO.ExamplesDTO;
-import Models.DTO.TopicsDTO;
-=======
 import Models.CrudUpdate;
 import Models.DTO.UpdateTableDTO;
->>>>>>> F2_2
+
 import Services.IHigherService;
 import Services.Impl.CRUD;
 import Services.Impl.HigherServiceImpl;
@@ -40,27 +34,7 @@ public class CRUD_Main {
 		
 		
 		
-		
-<<<<<<< HEAD
-//		System.out.println(serv.readLanguageTag("Bla").size());
-//		System.out.println(serv.readTopics("").size());
-//		System.out.println(serv.readExamples("").size());
-//		TopicsDTO dto = serv.getTopicsByLanguageId(5);
-//		System.out.println(dto.getMessage());
-//		for(int i = 0; i<10; i++) {
-//	
-//			System.out.println(dto.getTopics().get(i).title);
-//		}
-		ExamplesDTO dto = serv.getExamplesByTopicId(1);
-		System.out.println(dto.getMessage());
-		for(int i = 0; i<10; i++) {
-	
-			System.out.println(dto.getExamples().get(i).title);
-		}
-//		for(int i = 0; i<10; i++) {
-//			ExamplesDAL t = (ExamplesDAL) serv.getExamplesByTopicId(1).get(i);
-//			System.out.println(t.title);
-//		}
+
 		
 	}
 
