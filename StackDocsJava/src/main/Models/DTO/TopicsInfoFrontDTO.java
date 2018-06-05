d865d32d74edabdcec63e821019ed8a750591fff
PauliusZiedelis <p.ziedelis@gmail.com>
2018-06-05 13:22:23 +0300

    Topics+Topics Info

---
+++ StackDocsJava/src/main/Models/DTO/TopicsInfoFrontDTO.java
@@ -0,0 +1,46 @@
+package Models.DTO;
+import java.util.List;
+
+import Models.Topic;
+import Models.DAL.TopicsDAL;
+
+public class TopicsInfoFrontDTO {
+	private boolean _Succcess;
+	private String _Message;
+	private List<Topic> _TopicsInfo;
+	
+	TopicsInfoFrontDTO() {
+		
+	}
+	public TopicsInfoFrontDTO(boolean sucs, String msg, List<Topic> topicInfo){
+		_Succcess=sucs;
+		_Message=msg;
+		_TopicsInfo=topicInfo;
+	}
+
+	public boolean is_Succcess() {
+		return _Succcess;
+	}
+
+	public void set_Succcess(boolean sucs) {
+		this._Succcess = sucs;
+	}
+
+	public String get_Message() {
+		return _Message;
+	}
+
+	public void set_Message(String msg) {
+		this._Message = msg;
+	}
+
+	public List<Topic> get_Topics() {
+		return _TopicsInfo;
+	}
+
+	public void set_Topics(List<Topic> topicInfo) {
+		this._TopicsInfo = topicInfo;
+	}
+
+
+}
