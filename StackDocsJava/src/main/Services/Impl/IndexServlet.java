70d5ad23df03a2f7a6f06f82c3eeb52eb05323f9
CrossPC\Karolis <karolis.vas@gmail.com>
2018-06-05 15:11:36 +0300

    Working

--- StackDocsJava/src/main/Services/Impl/IndexServlet.java
+++ StackDocsJava/src/main/Services/Impl/IndexServlet.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+//import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
